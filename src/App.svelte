b170aa042e17aa625e67b70e419b97accde88c3d
twsnmp <ymi@twise.co.jp>
2024-01-05 06:20:30 +0900

    TWSNMP FCとの通信処理を追加

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,23 @@
 <script>
-  import { Alert } from 'flowbite-svelte';
+  import {TwsnmpAPI} from "./lib/twsnmpapi";
+  import { Alert,Button } from 'flowbite-svelte';
+  let loginOK = false;
+  let loginNG = false;
+  let api = new TwsnmpAPI("http://localhost:8080");
+  const login = async () => {
+    loginOK = await api.login("twsnmp","twsnmp");
+    loginNG = !loginOK;
+    const map = await api.get("/api/map");
+    console.log(map);
+  }
 </script>
 
 <div class="p-8">
-  <Alert>
-    <span class="font-medium">Info alert!</span>
-    Change a few things up and try submitting again.
-  </Alert>
+  {#if loginOK}
+   <Alert color="indigo">ログイン成功</Alert>
+  {/if}
+  {#if loginNG}
+    <Alert>ログイン失敗</Alert>
+  {/if}
+  <Button color="blue"  on:click={login}>ログイン</Button>
 </div>
---
+++ src/lib/twsnmpapi.ts
@@ -0,0 +1,85 @@
+
+export class TwsnmpAPI {
+  url: string
+  token: string
+  constructor(url: string) {
+    this.url = url;
+    this.token = '';
+  }
+  async login(user:string,password:string) :Promise<boolean>  {
+    try {
+      const res = await fetch(this.url + '/login', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          UserID: user,
+          Password: password,
+        })
+      })
+      if (res.status == 200) {
+        const r = await res.json();
+        console.log(r);
+        if (r && r.token ) {
+          this.token = r.token;
+          return true;
+        }
+      }
+      return false;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+  async get(api:string) :Promise<any> {
+    try {
+      const res = await fetch(this.url + api, {
+        method: 'GET',
+        headers: {
+        'Authorization': 'Bearer ' + this.token,
+        },
+      })
+      if (res.status != 200) {
+        return undefined;
+      }
+      return await res.json();
+    } catch (e) {
+      return undefined;
+    }
+  }
+  async post(api:string,data:any) :Promise<any> {
+    try {
+      const res = await fetch(this.url + api, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.token,
+        },
+        body: JSON.stringify(data),
+      })
+      if (res.status != 200) {
+        return undefined;
+      }
+      return await res.json();
+    } catch (e) {
+      console.log(e);
+      return undefined;
+    }
+  }
+  async delete(api:string): Promise<boolean> {
+    try {
+      const res = await fetch('APIURL'+ api, {
+        method: 'DELETE',
+        headers: {
+        'Authorization': 'Bearer ' + this.token,
+        },
+      })
+      return res.status == 204;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+}
+
