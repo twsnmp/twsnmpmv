164487cff37823cb296e96aa6845c5cc2333069f
twsnmp <ymi@twise.co.jp>
2024-01-06 07:22:54 +0900

    Capacitarを追加

---
+++ android/app/src/main/java/jp/co/twise/twsnmpmv/MainActivity.java
@@ -0,0 +1,5 @@
+package jp.co.twise.twsnmpmv;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jp.co.twise.twsnmpmv',
+  appName: 'twsnmpmv',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
