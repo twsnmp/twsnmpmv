b4442f35710fd9013f02635d7b4109603cfc6f45
twsnmp <ymi@twise.co.jp>
2024-01-18 04:57:23 +0900

    ボタンのアイコンサイズ調整

--- src/App.svelte
+++ src/App.svelte
@@ -39,7 +39,7 @@
       appBtnPosition="left"
       btnClass="text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
     >
-      <Icon path={icons.mdiListBox} size={2} />
+      <Icon path={icons.mdiListBox} size={1.5} />
       <Tooltip arrow={false}>リスト</Tooltip>
     </BottomNavItem>
     <BottomNavItem
@@ -48,7 +48,7 @@
       appBtnPosition="middle"
       btnClass="text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
     >
-      <Icon path={icons.mdiMapMarker} size={2} />
+      <Icon path={icons.mdiMapMarker} size={1.5} />
       <Tooltip arrow={false}>地図</Tooltip>
     </BottomNavItem>
     <div class="flex items-center justify-center">
@@ -58,7 +58,7 @@
         btnClass="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800 text-white"
         on:click={() => (showEditSite = true)}
       >
-        <Icon path={icons.mdiPlus} size={2} />
+        <Icon path={icons.mdiPlus} size={1.5} />
         <Tooltip arrow={false}>追加</Tooltip>
       </BottomNavItem>
     </div>
@@ -67,7 +67,7 @@
       appBtnPosition="middle"
       btnClass="text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
     >
-      <Icon path={icons.mdiCog} size={2} />
+      <Icon path={icons.mdiCog} size={1.5} />
       <Tooltip arrow={false}>設定</Tooltip>
     </BottomNavItem>
     <BottomNavItem btnName="ダーク" appBtnPosition="right">
