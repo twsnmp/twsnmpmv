c0ac2e7fbf0e456d77aa097230dd13316c835bfc
twsnmp <ymi@twise.co.jp>
2024-01-25 06:38:25 +0900

    不要パッケージの削除、npm build時のワーニングを解決

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit:4096,
+  },
   plugins: [svelte()],
 })
