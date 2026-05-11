06fe1e643b21a86fe660cb048a009999f5341775
twsnmp <ymi@twise.co.jp>
2026-05-11 16:04:45 +0900

    chore: suppress accessibility warnings from node_modules in build

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,10 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: [vitePreprocess({})],
+  onwarn: (warning, handler) => {
+    if (warning.code.startsWith("a11y-") && warning.filename?.includes("node_modules")) {
+      return;
+    }
+    handler(warning);
+  },
 };
